a25b9b14beef25262c785f7eaf2585f753564a7f
Paul Kawalya <appcellon@gmail.com>
2026-03-22 23:51:40 +0300

    Remove paper plane icon from Send Anonymously button

--- resources/views/livewire/floating-suggestion-box.blade.php
+++ resources/views/livewire/floating-suggestion-box.blade.php
@@ -122,11 +122,6 @@
                         <button type="submit" class="btn-submit" wire:loading.attr="disabled" wire:target="submit">
                             <span wire:loading.remove wire:target="submit"
                                 style="display: flex; align-items: center; gap: 8px; justify-content: center;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" stroke-width="2">
-                                    <line x1="22" y1="2" x2="11" y2="13" />
-                                    <polygon points="22 2 15 22 11 13 2 9 22 2" />
-                                </svg>
                                 Send Anonymously
                             </span>
                             <span wire:loading wire:target="submit"
