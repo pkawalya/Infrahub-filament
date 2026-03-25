4e834a2a584fec5fa97ffa31122cc0b19cdf5327
Paul Kawalya <appcellon@gmail.com>
2026-03-25 15:35:11 +0300

    feat: Create Fuel Log opens as full-page modal (5xl width, not slide-over)

--- app/Filament/App/Resources/EquipmentFuelLogs/Pages/ManageEquipmentFuelLogs.php
+++ app/Filament/App/Resources/EquipmentFuelLogs/Pages/ManageEquipmentFuelLogs.php
@@ -13,7 +13,9 @@ class ManageEquipmentFuelLogs extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->slideOver(false)
+                ->modalWidth('5xl'),
         ];
     }
 }
